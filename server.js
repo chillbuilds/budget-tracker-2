899c568cd633d2e3e5363f6624915b2a20ee220b
heliumface770 <heliumface770@gmail.com>
2020-03-07 15:55:23 -0700

    app functionality working

--- server.js
+++ server.js
@@ -9,18 +9,12 @@ const app = express();
 app.use(logger("dev"));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-
 app.use(express.static("public"));
 
 var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines"
 
-// mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/budget", {
-//   useNewUrlParser: true,
-//   useFindAndModify: false
-// });
 mongoose.connect(MONGODB_URI);
 
-// routes
 app.use(require("./routes/api.js"));
 
 app.listen(PORT, () => {
